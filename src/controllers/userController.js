772f0ba23718723bf6c0395e0f9a4d98346b9b34
Eun Din <escience1202@gmail.com>
2022-04-03 04:59:04 +0900

    make logout

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -133,7 +133,9 @@ export const finishGithubLogin = async (req, res) => {
   }
 };
 
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Delete User");
-export const logout = (req, res) => res.send("Log Out");
 export const see = (req, res) => res.send("See User");
