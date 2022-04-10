132935fa5ddc8382acc1686065a006165139cf0f
Eun Din <escience1202@gmail.com>
2022-04-10 23:21:51 +0900

    change password

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -4,6 +4,8 @@ import {
   see,
   startGithubLogin,
   finishGithubLogin,
+  getChangePassword,
+  postChangePassword,
   getEdit,
   postEdit,
 } from "../controllers/userController";
@@ -13,6 +15,11 @@ const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter
+  .route("/change-password")
+  .all(protectorMiddleware)
+  .get(getChangePassword)
+  .post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/:id", see);
