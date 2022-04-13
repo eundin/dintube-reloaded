0ee716ed2aecdda0383ce596c240d9d9270f9370
Eun Din <escience1202@gmail.com>
2022-04-13 23:51:22 +0900

    video owner Connect user and video, fix pw bug

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/User";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
+import Video from "../models/Video";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
@@ -202,12 +203,14 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id);
+  const user = await User.findById(id).populate("videos");
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
   }
+  const videos = await Video.find({ owner: user._id });
   return res.render("users/profile", {
-    pageTitle: "user.name",
+    pageTitle: user.name,
     user,
+    videos,
   });
 };
