0ebbfda34c0ed801327339fd055645419d153316
Eun Din <escience1202@gmail.com>
2022-04-19 19:10:03 +0900

    webpack, styles

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,8 +2,9 @@ import User from "../models/User";
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({}).sort({ createdAt: "desc" });
-  console.log(videos);
+  const videos = await Video.find({})
+    .sort({ createdAt: "desc" })
+    .populate("owner");
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = async (req, res) => {
@@ -104,7 +105,7 @@ export const search = async (req, res) => {
       title: {
         $regex: new RegExp(`${keyword}$`, "i"),
       },
-    });
+    }).populate("owner");
   }
   return res.render("search", { pageTitle: "Search", videos });
 };
